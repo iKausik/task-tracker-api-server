28a44b3b985ea53fe97d472f0e9ebfa5c3b9911c
Kausik Das <kausik007007@gmail.com>
2021-05-12 21:19:25 +0530

    database credentials updated

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const Pool = require("pg").Pool;
+const { Pool, Client } = require("pg");
 
 const pool = new Pool({
   user: process.env.HEROKU_PSQL_USER || "postgres",
@@ -7,5 +7,22 @@ const pool = new Pool({
   port: process.env.HEROKU_PSQL_PORT || 5432,
   database: process.env.HEROKU_PSQL_DATABASE || "task-tracker",
 });
+
+const client = new Client({
+  user: process.env.HEROKU_PSQL_USER || "postgres",
+  password: process.env.HEROKU_PSQL_PASSWORD || "hello@sql",
+  host: process.env.HEROKU_PSQL_HOST || "localhost",
+  port: process.env.HEROKU_PSQL_PORT || 5432,
+  database: process.env.HEROKU_PSQL_DATABASE || "task-tracker",
+});
+client.connect();
+
+client.query(
+  "CREATE TABLE tasks(id SERIAL PRIMARY KEY, text VARCHAR(255) NOT NULL, day VARCHAR(255) NOT NULL, reminder VARCHAR(255) NOT NULL)",
+  (err, res) => {
+    console.log(err, res);
+    client.end();
+  }
+);
 
 module.exports = pool;
